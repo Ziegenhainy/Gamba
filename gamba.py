c31370e2a83a03cd280d17e3b6b7f397fc6de232
Ziegenhainy <hawipru@gmail.com>
2026-03-02 16:23:01 +0100

    added money

--- gamba.py
+++ gamba.py
@@ -4,24 +4,41 @@ import itertools as it
 
 
 def main():
-    randomSlots = [rd.randint(0, 100) for _ in range(9)]
+    money = 1000
     
-    delay = 0.1
+    while True:
+        bet = -1
+        while bet == -1:
+            betQuery = input(f"\x1b[2KYou have {money}$ left, how much do you wanna gamba? ")
+            try:
+                bet = int(betQuery)
+            except ValueError:
+                print("\033[91mYOU ARE STUPID! ENTER A NUMBER!", end="\033[37m\r\033[A")
 
-    for i in it.count():
-        if i >= 20:
-            delay *= 1+rd.random()*0.5
-        print("[", end="")
-        randomSlots.pop(0)
-        randomSlots.append(rd.randint(0,100))
-        for n in randomSlots:
-            print(str(n).center(4), end="|")
-        print("\b]")
-        print("                      ^^", end="\r\033[A")
-        t.sleep(delay)
-        if delay > 1.5:
-            break
-    print(f"\n\nYou Won {randomSlots[4]}!")
+            if money < bet:
+                print("\033[91mYOU ARE BROKE! BET LESS!", end="\033[37m\r\033[A")
+                bet = -1
+
+        randomSlots = [rd.randint(0, 100) for _ in range(9)]
+        delay = 0.1
+        money -= bet
+        for i in it.count():
+            if i >= 20:
+                delay *= 1+rd.random()*0.5
+            print("[", end="")
+            randomSlots.pop(0)
+            randomSlots.append(rd.randint(0,100))
+            for n in randomSlots:
+                print(str(n).center(4), end="|")
+            print("\b]")
+            print("                      ^^", end="\r\033[A")
+            t.sleep(delay)
+            if delay > 1.5:
+                break
+
+        winnings = int(bet * (randomSlots[4]/50))
+        print(f"\n\nYou Won {winnings}$!")
+        money += winnings
 
 if __name__ == "__main__":
     main()
